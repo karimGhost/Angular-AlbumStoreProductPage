e67891784f9388822c56888d2b8b69bdbdbf805a
karimGhost <abdulkarimkassimsalim@gmail.com>
2021-11-20 05:46:50 +0300

    'AlbumStoreProductPage'

---
+++ src/app/product.service.ts
@@ -0,0 +1,20 @@
+import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { Album } from './album';
+import { Response, Http} from '@angular/http';
+import { Product } from './product';
+export class ProductService {
+  private _albumUrl =  '../assets/album.json';
+private _productsUrl = '../assets/products.json';
+  constructor( private _http: Http){
+  }  
+ getAlbum(id: number): Observable<Album>{
+   return this._http.get(this._albumUrl).map(response => <Album>response.json());
+ }
+
+getProducts(): Observable<Product[]>{
+  return this._http.get(this._productsUrl).map(response => <Product[]>response.json());
+}
+}
+
